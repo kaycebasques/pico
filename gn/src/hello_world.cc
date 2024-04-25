d836457ad31ba8634bf586390f6625aa7f3e25fb
Kayce Basques <kayce@google.com>
2024-04-24 18:52:25 -0700

    BLINKY IN USERAPPINIT!!

--- gn/src/hello_world.cc
+++ gn/src/hello_world.cc
@@ -11,46 +11,69 @@
 #include "pw_thread_freertos/context.h"
 #include "pw_thread_freertos/options.h"
 
-namespace {
+// namespace {
+// 
+// static constexpr size_t kDisplayDrawThreadStackWords = 512;
+// static pw::thread::freertos::StaticContextWithStack<
+//     kDisplayDrawThreadStackWords>
+//     display_draw_thread_context;
+// 
+// const pw::thread::Options& DisplayDrawThreadOptions() {
+//   static constexpr auto options =
+//       pw::thread::freertos::Options()
+//           .set_name("DisplayDrawThread")
+//           .set_static_context(display_draw_thread_context)
+//           // TODO: amontanez - Find a way to better manage priorities.
+//           .set_priority(static_cast<UBaseType_t>(tskIDLE_PRIORITY + 1));
+//   return options;
+// }
 
-static constexpr size_t kDisplayDrawThreadStackWords = 512;
-static pw::thread::freertos::StaticContextWithStack<
-    kDisplayDrawThreadStackWords>
-    display_draw_thread_context;
-
-const pw::thread::Options& DisplayDrawThreadOptions() {
-  static constexpr auto options =
-      pw::thread::freertos::Options()
-          .set_name("DisplayDrawThread")
-          .set_static_context(display_draw_thread_context)
-          // TODO: amontanez - Find a way to better manage priorities.
-          .set_priority(static_cast<UBaseType_t>(tskIDLE_PRIORITY + 1));
-  return options;
-}
-
-void MainTask(void*) {
-  PW_LOG_INFO("Hello, world!");
-  const uint LED_PIN = 25;
-  const uint WAIT_MS = 1000;
-  gpio_init(LED_PIN);
-  while (1) {
-    gpio_put(LED_PIN, 1);
-    sleep_ms(WAIT_MS);
-    gpio_put(LED_PIN, 0);
-    sleep_ms(WAIT_MS);
-  }
-}
-
-}  // namespace
+// void MainTask(void*) {
+//   PW_LOG_INFO("Hello, world!");
+//   const uint LED_PIN = 25;
+//   const uint WAIT_MS = 1000;
+//   gpio_init(LED_PIN);
+//   while (1) {
+//     gpio_put(LED_PIN, 1);
+//     sleep_ms(WAIT_MS);
+//     gpio_put(LED_PIN, 0);
+//     sleep_ms(WAIT_MS);
+//   }
+// }
+//
+// }  // namespace
 
 namespace pw::system {
+
+// void UserAppInit() {
+//   const uint LED_PIN = 25;
+//   gpio_init(LED_PIN);
+//   gpio_put(LED_PIN, 1);
+//   PW_LOG_INFO("UserAppInit");
+//   // pw::thread::DetachedThread(DisplayDrawThreadOptions(), MainTask);
+// }
 
 void UserAppInit() {
   const uint LED_PIN = 25;
   gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
   gpio_put(LED_PIN, 1);
-  PW_LOG_INFO("UserAppInit");
-  // pw::thread::DetachedThread(DisplayDrawThreadOptions(), MainTask);
+  while (1) {
+    // gpio_put(LED_PIN, 1);
+    // for (volatile uint32_t i = 0; i < 100000000; i++) {
+    //   __asm__("nop");
+    // }
+    // gpio_put(LED_PIN, 0);
+    // for (volatile uint32_t i = 0; i < 100000000; i++) {
+    //   __asm__("nop");
+    // }
+  }
+  // stdio_usb_init();
+  // PW_LOG_INFO("pw_system main");
+  // pw::system::Init();
+  // pw::system::GetRpcServer().RegisterService(kudzu_service);
+  // vTaskStartScheduler();
+  // PW_UNREACHABLE;
 }
 
 }  // namespace pw::system
