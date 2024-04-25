d1d1be547363aa6e010b7e2c3b087efe3b868c22
Kayce Basques <kayce@google.com>
2024-04-24 18:59:21 -0700

    GENUINE BLINKY WITH PICO SDK API!!!!!!!!!!!!!

--- gn/src/hello_world.cc
+++ gn/src/hello_world.cc
@@ -31,15 +31,15 @@ const pw::thread::Options& DisplayDrawThreadOptions() {
 void MainTask(void*) {
   // PW_LOG_INFO("Hello, world!");
   const uint LED_PIN = 25;
-  // const uint WAIT_MS = 1000;
+  const uint WAIT_MS = 1000;
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
-  gpio_put(LED_PIN, 1);
+  // gpio_put(LED_PIN, 1);
   while (1) {
-    // gpio_put(LED_PIN, 1);
-    // sleep_ms(WAIT_MS);
-    // gpio_put(LED_PIN, 0);
-    // sleep_ms(WAIT_MS);
+    gpio_put(LED_PIN, 1);
+    sleep_ms(WAIT_MS);
+    gpio_put(LED_PIN, 0);
+    sleep_ms(WAIT_MS);
   }
 }
 
